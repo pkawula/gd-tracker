43cb09ac152b8595327a04ac761e5b230c4e8146
Piotr Kawula <kontakt@piotrkawula.pl>
2025-11-10 12:29:45 +0100

    Initial commit

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+export type MeasurementType = "fasting" | "1hr_after_meal";
+
+export interface GlucoseReading {
+	id: string;
+	user_id: string;
+	glucose_value: number;
+	measurement_type: MeasurementType;
+	measured_at: string;
+	comment?: string;
+	created_at: string;
+}
+
+export interface DateRange {
+	from: Date;
+	to: Date;
+}
+
+export const TARGET_RANGES = {
+	fasting: 91,
+	"1hr_after_meal": 140,
+} as const;
